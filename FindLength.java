220bbf9fbd240ed4735e4ee909d156819a2a7cb1
ayushpasi28 <61745106+ayushpasi28@users.noreply.github.com>
2021-07-27 21:22:30 +0530

    Add files via upload

---
+++ FindLength.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class FindLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Ayush";
+		int length=0;
+		char a[]=s.toCharArray();
+		for(char c:a)
+		{
+			length++;
+		}
+         System.out.println(length);
+
+	}
+
+}
